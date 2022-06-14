44451367641df906a4d2e2516c4e418fb46122fc
هاني عبدالوهاب الموسى <halmousa@tvtc.gov.sa>
2022-06-14 10:16:45 +0300

    Add project files.

---
+++ FilePondUploader/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilePondUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveFile()
+        {
+            string fileName = "";
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+                var id = Request.Headers["id"];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    fileName = file.FileName;
+                    file.SaveAs(Server.MapPath($"/Content/{file.FileName}"));
+                }
+            }
+
+            return Json(fileName);
+        }
+
+        public ActionResult DeleteFile()
+        {
+            var id = Request.Headers["id"];
+
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    file.SaveAs(Server.MapPath($"/Content/{file.FileName}"));
+                }
+            }
+
+            return Json(true);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
